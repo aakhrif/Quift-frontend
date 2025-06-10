7b5794f8fcd5b805c094ef53c9615593c2450a9a
spectres <amine@post.com>
2025-06-10 10:51:00 +0200

    Configure api url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
   ssr: true,
+  devServer: {
+    port: 3000,
+    host: '0.0.0.0', // WICHTIG!
+  },
   css: ['@/assets/css/tailwind.css'],
 
   vite: {
@@ -13,7 +17,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
    public: {
-     apiUrl: process.env.API_URL || 'http://localhost:8080'
+     apiUrl: process.env.API_BASE_URL || 'http://localhost:8080'
    }
  },
 
